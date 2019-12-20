af79203eabefbaaad9ce59d09693549748a12f69
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-20 01:56:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+class School 
+
+
+
+def initialize(name)
+  @name = name
+  @roster = Hash.new
+   
+  end   
+def roster
+  @roster
+end 
+def add_student(key, value)
+  @roster[key] = []
+  @roster[key] << value 
+end  
