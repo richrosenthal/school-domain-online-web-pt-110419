d3fee79ef3303bfd2d92fff7e8afe8aca30cd97f
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-20 02:40:33 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+class School 
+
+
+
+def initialize(name)
+  @name = name
+  @roster = Hash.new
+   
+  end   
+def roster
+  @roster
+end 
+def add_student(value, key)
+  if roster.include?(key)
+    roster[key] << value 
+  else
+  roster[key] = []
+  roster[key] << value 
+end
+end  
+
+def grade(key)
+  roster[key]
+end
+
+def sort
+ roster.each do |key, value| 
+  value.sort!
+ end
+end
+
+
+
+
+end 
